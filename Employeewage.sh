565d4c357b6c038d53004835afb5d298cc866943
AnushaManda-27 <anushamanda27@gmail.com>
2021-04-28 18:20:02 +0530

    added file

--- Employeewage.sh
+++ Employeewage.sh
@@ -2,3 +2,11 @@
 
 
 echo "welcome to Employeewage"
+IS_PRESENT=1
+empCheck=$(( RANDOM % 2 ))
+if [ $empCheck -eq $IS_PRESENT ]
+then
+    echo "Employee is present"
+else
+     echo "Employee is abscent"
+fi
