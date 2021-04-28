c1158916328547386c852a33bf5e84cc947c67fe
AnushaManda-27 <anushamanda27@gmail.com>
2021-04-28 18:54:33 +0530

    added computation

--- Employeewage.sh
+++ Employeewage.sh
@@ -1,4 +1,12 @@
 #! /bin/bash -x
+IS_PRESENT=1
+EMP_RATE_PER_HR=20
+empCheck=$(( RANDOM % 2 ))
+if [ $empCheck -eq $IS_PRESENT ]
+then
+     empHrs=8
+else
+     empHrs=0
+fi
+salary=$(( EMP_RATE_PER_HR * empHrs ))
 
-
-echo "welcome to Employeewage"
