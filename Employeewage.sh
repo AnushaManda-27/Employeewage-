74421806871ae92f9eeed6542ebe8c38f47945d4
AnushaManda-27 <anushamanda27@gmail.com>
2021-04-30 11:02:54 +0530

    Added Employeewage using Function

--- Employeewage.sh
+++ Employeewage.sh
@@ -2,23 +2,27 @@
 IS_PRESENT_FULL=1
 IS_PRESENT_HALF=2
 EMP_RATE_PER_HR=20
-MAX_WORKING_DAYS=20
-NO_OF_WRKINHG_DAYS=5
-MAX_WORKING_HRS=85
+NO_OF_WORKING_DAYS=5
+MAX_WORK_HRS=20
 totalworkinghrs=0
 totalworkingdays=0
-
-while [ $totalworkingdays -lt $MAX_WORKING_DAYS ] && [ $totalworkinghrs -lt $MAX_WORK_HRS ]
+function getworkHours(){
+        local empCheck=$1
+        local empHrs=0
+        case  $empCheck in
+            $IS_PRESENT_FULL)empHrs=8;;
+            $IS_PRESENT_HALF)empHrs=4;;
+            *)empHrs=0;;
+        esac
+        echo $empHrs
+}
+while [ $totalworkingdays -lt $NO_OF_WORKING_DAYS ] && [ $totalworkinghrs -lt $MAX_WORK_HRS ]
 do
         empCheck=$(( RANDOM % 3 ))
         (( totalworkingdays++))
-        case  $empCheck in
-        $IS_PRESENT_FULL)empHrs=8;;
-       $IS_PRESENT_HALF)empHrs=4;;
-                      *)empHrs=0;;
-        esac
+        empHrs="$( getworkHours $empCheck)"
         totalworkinghrs=$(( totalworkinghrs + empHrs ))
         #salary=$(( $EMP_RATE_PER_HR * $empHrs ))
 done
-salary=$(( totalworkinghrs * EMP_RATE_PER_HR))
+salary=$(( $totalworkinghrs * $EMP_RATE_PER_HR))
 
