d4313c388dde55e0a148fe935bb1e85d91856c3f
AnushaManda-27 <anushamanda27@gmail.com>
2021-04-30 07:54:16 +0530

    added part time employeewage

--- Employeewage.sh
+++ Employeewage.sh
@@ -1,12 +1,15 @@
 #! /bin/bash -x
-IS_PRESENT=1
+IS_PRESENT_FULL=1
+IS_PRESENT_HALF=2
 EMP_RATE_PER_HR=20
-empCheck=$(( RANDOM % 2 ))
-if [ $empCheck -eq $IS_PRESENT ]
+empCheck=$(( RANDOM % 3 ))
+if [ $empCheck -eq $IS_PRESENT_FULL ]
 then
      empHrs=8
+elif [ $empCheck -eq $IS_PRESENT_HALF ]
+then
+     empHrs=4
 else
      empHrs=0
 fi
 salary=$(( EMP_RATE_PER_HR * empHrs ))
-
