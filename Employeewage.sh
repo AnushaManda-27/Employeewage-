cad6aa9a87e40fa8a3822171760894cd9764bb8d
AnushaManda-27 <anushamanda27@gmail.com>
2021-04-30 10:47:38 +0530

    Added Employeewage using while loop

--- Employeewage.sh
+++ Employeewage.sh
@@ -2,16 +2,23 @@
 IS_PRESENT_FULL=1
 IS_PRESENT_HALF=2
 EMP_RATE_PER_HR=20
-MAX_wORKING_DAYS=20
+MAX_WORKING_DAYS=20
+NO_OF_WRKINHG_DAYS=5
+MAX_WORKING_HRS=85
+totalworkinghrs=0
+totalworkingdays=0
 
-for(( counter=1; counter<=$MAX_WORKING_DAYS; counter++ ))
+while [ $totalworkingdays -lt $MAX_WORKING_DAYS ] && [ $totalworkinghrs -lt $MAX_WORK_HRS ]
 do
         empCheck=$(( RANDOM % 3 ))
-
-        case  $empcheck in
+        (( totalworkingdays++))
+        case  $empCheck in
         $IS_PRESENT_FULL)empHrs=8;;
        $IS_PRESENT_HALF)empHrs=4;;
                       *)empHrs=0;;
         esac
-        salary=$(( $EMP_RATE_PER_HR * $empHrs ))
+        totalworkinghrs=$(( totalworkinghrs + empHrs ))
+        #salary=$(( $EMP_RATE_PER_HR * $empHrs ))
 done
+salary=$(( totalworkinghrs * EMP_RATE_PER_HR))
+
