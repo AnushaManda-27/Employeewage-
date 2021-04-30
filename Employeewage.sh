d4278541cd93f2f6624cf0f1b6a17325f3bdb6c5
AnushaManda-27 <anushamanda27@gmail.com>
2021-04-30 12:13:55 +0530

    Added Employewage using dictionary

--- Employeewage.sh
+++ Employeewage.sh
@@ -1,30 +1,46 @@
 #! /bin/bash -x
-IS_PRESENT_FULL=1
-IS_PRESENT_HALF=2
-EMP_RATE_PER_HR=20
-NO_OF_WORKING_DAYS=20
-MAX_WORK_HRS=20
-totalworkinghrs=0
-totalworkingdays=0
-function getworkHours(){
-        local empCheck=$1
-        local empHrs=0
-        case  $empCheck in
-            $IS_PRESENT_FULL)empHrs=8;;
-            $IS_PRESENT_HALF)empHrs=4;;
-            *)empHrs=0;;
-        esac
-        echo $empHrs
-}
-while [ $totalworkingdays -lt $NO_OF_WORKING_DAYS ] && [ $totalworkinghrs -lt $MAX_WORK_HRS ]
-do
-        empCheck=$(( RANDOM % 3 ))
-        (( totalworkingdays++))
-        empHrs="$( getworkHours $empCheck)"
-        totalworkinghrs=$(( totalworkinghrs + empHrs ))
-        #salary=$(( $EMP_RATE_PER_HR * $empHrs ))
-        dailywage[$totalworkingdays]=$(( $empHrs * $EMP_RATE_PER_HR ))
-done
-salary=$(( $totalworkinghrs * $EMP_RATE_PER_HR))
-echo ${dailywage[@]}
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=80;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
 
+#VARIABLES
+ totalEmpHr=0;
+totalWorkingDays=0;
+
+declare -A dailyWage
+
+function getWorkHrs(){
+local empCheck=$1;
+case $empCheck in $IS_FULL_TIME)
+empHrs=8 ;;
+$IS_PART_TIME)empHrs=4 ;;
+*)empHrs=0;;
+
+esac
+echo $empHrs
+
+}
+
+function getEmpWage() {
+local empHr=$1
+echo $(($empHr*$EMP_RATE_PER_HR))
+
+}
+
+while [[  $totalEmpHr -lt $MAX_HRS_IN_MONTH &&
+ $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+
+do
+
+
+ ((totalWorkingDays++))
+ empCheck=$((RANDAM%3));
+ empHrs="$( getWorkHrs $empCheck )"
+ totalEmpHrs=$(($totalEmpHr+$empHrs))
+dailyWage["Day $totalWorkingDays"]="$( getEmpWage $empHrs )"
+
+done
+ totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
+echo  ${dailyWage[@]}
+echo ${!dailyWage[@]}
