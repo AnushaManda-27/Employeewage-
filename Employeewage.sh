1d76b03be0dde50f6071f30ca8d847b237771916
AnushaManda-27 <anushamanda27@gmail.com>
2021-04-30 10:36:40 +0530

    added Employeewage using for loop

--- Employeewage.sh
+++ Employeewage.sh
@@ -2,14 +2,16 @@
 IS_PRESENT_FULL=1
 IS_PRESENT_HALF=2
 EMP_RATE_PER_HR=20
-empCheck=$(( RANDOM % 3 ))
-if [ $empCheck -eq $IS_PRESENT_FULL ]
-then
-     empHrs=8
-elif [ $empCheck -eq $IS_PRESENT_HALF ]
-then
-     empHrs=4
-else
-     empHrs=0
-fi
-salary=$(( EMP_RATE_PER_HR * empHrs ))
+MAX_wORKING_DAYS=20
+
+for(( counter=1; counter<=$MAX_WORKING_DAYS; counter++ ))
+do
+        empCheck=$(( RANDOM % 3 ))
+
+        case  $empcheck in
+        $IS_PRESENT_FULL)empHrs=8;;
+       $IS_PRESENT_HALF)empHrs=4;;
+                      *)empHrs=0;;
+        esac
+        salary=$(( $EMP_RATE_PER_HR * $empHrs ))
+done
